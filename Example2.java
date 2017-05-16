6cd36b1e2d3098cda46d1a695add82df2b9389f8
John Moore <johnmoore314@gmail.com>
2017-05-16 16:50:56 -0500

    Add Example2.java with a class that teaches about variables.

---
+++ Example2.java
@@ -0,0 +1,16 @@
+// this program will demonstrate a variable
+class Example2 {
+  public static void main(String[] args){
+    int var1; // this declares a variable
+    int var2; // another variable
+
+    var1 = 1024; // variable assignment
+
+    System.out.println("var1 contains " + var1);
+
+    var2 = var1 / 2;
+
+    System.out.print("var2 contains var1 / 2: ");
+    System.out.println(var2);
+  }  
+}
