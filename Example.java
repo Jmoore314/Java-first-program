99d6e04106abf819b2b842ce055883c1238de89e
John Moore <johnmoore314@gmail.com>
2017-05-16 16:18:05 -0500

    Add Example.java with an Example class that uses system output.

---
+++ Example.java
@@ -0,0 +1,14 @@
+/*
+  This is a simple Java Program.
+
+  call this file Example.java
+*/
+
+class Example {
+  // Java begins with a call to main().
+  public static void main(String args[]) {
+    System.out.println("Java drives the Web.");
+    System.out.println("Hello World");
+  }
+}
+
